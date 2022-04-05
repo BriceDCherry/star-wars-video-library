aecdee769c6c742fa7f17635216b7fe1e4b0ee2a
bcherry4 <bcherry4@ford.com>
2022-04-05 13:26:48 -0600

    Add character sets

--- site/movie.js
+++ site/movie.js
@@ -10,10 +10,31 @@ fetch(`https://swapi.dev/api/films/${queryParams.get("episode_id")}`)
     .then(response => {
         return response.json()
     }).then(parsedResponse => {
-            const p = document.createElement("p")
-            p.textContent = parsedResponse.title
-            main.append(p)
-            
-    })
+        const p = document.createElement("p")
+        p.innerHTML = `
+            <div class ="movie">
+                <a href="movie.html?movie=movie-id-goes-here">${parsedResponse.title}</a>
+                <time>${parsedResponse.release_date.substring(0, 4)}</time>
+                
 
-    //console.log(response.json())
+
+            </div>
+            
+            `
+        main.append(p)
+
+    })
+fetch(`https://swapi.dev/api/films/${queryParams.get("episode_id")}`)
+    .then(response => {
+        return response.json()
+
+    }).then(parsedResponse => {
+        parsedResponse.characters.forEach(url => fetch(url).then(response => {
+            return response.json()}
+        ).then(parsedResponse => {
+            const li = document.createElement("li")
+            li.textContent = parsedResponse.name
+            const ul = document.querySelector("ul")
+            ul.append(li)
+        }))
+    })
